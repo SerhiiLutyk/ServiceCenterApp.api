fc4ad0cd65f71c1c4b5fdadcbe45fe5b95c29fd6
sluti <serhiiliutyk123@gmail.com>
2025-06-18 20:40:29 +0300

    🧱 Add RepairDbContext and design-time factory with PostgreSQL configuration
    - Implemented DbContext with DbSet properties for all entities
    - Registered Fluent API configurations in OnModelCreating
    - Added RepairDbContextFactory for EF Core tooling and migrations
    - Configured connection string for PostgreSQL

---
+++ ServiceCenterAppDalEF/DbCreating/RepairDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceCenterAppDalEF.Configurations;
+using ServiceCenterAppDalEF.Entities;
+
+public class RepairDbContext : DbContext
+{
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<RepairType> RepairTypes { get; set; }
+    public DbSet<AdditionalService> AdditionalServices { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+
+    public RepairDbContext(DbContextOptions<RepairDbContext> options)
+        : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ClientConfiguration());
+        modelBuilder.ApplyConfiguration(new OrderConfiguration());
+        modelBuilder.ApplyConfiguration(new RepairTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new AdditionalServiceConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentConfiguration());
+    }
+}
---
+++ ServiceCenterAppDalEF/DbCreating/RepairDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ServiceCenterAppDalEF
+{
+    public class RepairDbContextFactory : IDesignTimeDbContextFactory<RepairDbContext>
+    {
+        public RepairDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<RepairDbContext>();
+
+            optionsBuilder.UseNpgsql("Host=localhost;Database=ServiceDb;Username=postgres;Password=yourpassword");
+
+            return new RepairDbContext(optionsBuilder.Options);
+        }
+    }
+}
