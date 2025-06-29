b36b56136850d582d9da24059591f284c9a57e0f
sluti <serhiiliutyk123@gmail.com>
2025-06-30 01:32:31 +0300

    🔗 Add UnitOfWork pattern
    - Introduced IUnitOfWork and UnitOfWork implementation
    - Exposes all repositories and single SaveChangesAsync entry point
    - Registered UnitOfWork in dependency injection container

---
+++ ServiceCenterAppDalEF/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RepairServiceDAL.Repositories.Interfaces;
+
+namespace ServiceCenterAppDalEF.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IClientRepository Clients { get; }
+        IOrderRepository Orders { get; }
+        IRepairTypeRepository RepairTypes { get; }
+        IAdditionalServiceRepository AdditionalServices { get; }
+        IPaymentRepository Payments { get; }
+
+        Task<int> SaveAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ ServiceCenterAppDalEF/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using RepairServiceDAL.Repositories;
+using RepairServiceDAL.Repositories.Interfaces;
+using ServiceCenterAppDalEF.Interfaces;
+using ServiceCenterAppDalEF.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepairServiceDAL.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RepairDbContext _context;
+
+        public UnitOfWork(RepairDbContext context)
+        {
+            _context = context;
+        }
+
+        public IClientRepository Clients => new ClientRepository(_context);
+        public IOrderRepository Orders => new OrderRepository(_context);
+        public IRepairTypeRepository RepairTypes => new RepairTypeRepository(_context);
+        public IAdditionalServiceRepository AdditionalServices => new AdditionalServiceRepository(_context);
+        public IPaymentRepository Payments => new PaymentRepository(_context);
+
+        public async Task<int> SaveAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public void Dispose() => _context.Dispose();
+
+    }
+}
